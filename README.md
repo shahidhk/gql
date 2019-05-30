e89da563429309240f59fa89ceee36c20d53aa00
Shahidh K Muhammed <shahidh@hasura.io>
2019-05-30 11:51:21 +0530

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # gql
 A (WIP) client library for GraphQL written in Go with codegen for GraphQL types
+
+```go
+package main
+
+import (
+    "fmt"
+    "log"
+
+	"github.com/shahidhk/gql"
+)
+
+type Author struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+type Authors struct {
+	Authors []Authors `json:"authors"`
+}
+
+func main() {
+	client := gql.NewClient("http://localhost:8080/v1/graphql", nil)
+	var authors Authors
+	err := client.Execute(gql.Request{Query: `query { authors {id name}}`}, &authors)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(authors)
+}
+```
--- gql.go
+++ gql.go
@@ -4,12 +4,14 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 // Request is the GraphQL request containing Query and Variables
 type Request struct {
-	Query     string                 `json:"query"`
-	Variables map[string]interface{} `json:"variables"`
+	Query         string                 `json:"query"`
+	Variables     map[string]interface{} `json:"variables"`
+	OperationName string                 `json:"operationName,omitempty"`
 }
 
 // Response is the response from GraphQL server
@@ -20,18 +22,34 @@ type Response struct {
 
 // Error is a the GraphQL error from server
 type Error struct {
-	Message   string          `json:"message"`
-	Locations []ErrorLocation `json:"locations"`
-	Type      string          `json:"type"`
-	Path      []interface{}   `json:"path"`
+	Message    string           `json:"message"`
+	Locations  []ErrorLocation  `json:"locations"`
+	Type       string           `json:"type"`
+	Path       []interface{}    `json:"path"`
+	Extensions HasuraExtensions `json:"extensions"`
+}
+
+// Error returns the error message
+func (e Error) Error() string {
+	return e.Message
+}
+
+// HasuraExtensions is the error extension by Hasura
+type HasuraExtensions struct {
+	Path string `json:"path"`
+	Code string `json:"code"`
 }
 
 // Errors are an array of GraphQL errors
 type Errors []Error
 
-// Error returns the error message
-func (e Error) Error() string {
-	return e.Message
+// Error returns all error messages from Errors object
+func (e Errors) Error() string {
+	errors := []string{}
+	for _, err := range e {
+		errors = append(errors, err.Message)
+	}
+	return strings.Join(errors, ", ")
 }
 
 // ErrorLocation is the location of error in the query string
@@ -56,9 +74,26 @@ func NewClient(endpoint string, headers map[string]string) *Client {
 	}
 }
 
+// SetHeader sets a new header on the client
+func (c *Client) SetHeader(key, value string) {
+	c.Headers[key] = value
+}
+
+// ExecuteWithHeaders makes a request with extra headers
+func (c *Client) ExecuteWithHeaders(r Request, headers map[string]string, data interface{}) error {
+	for k, v := range headers {
+		c.SetHeader(k, v)
+	}
+	err := c.Execute(r, data)
+	for k := range headers {
+		delete(c.Headers, k)
+	}
+	return err
+}
+
 // Execute executes the Request r using the Client c and returns an error
 // Response data and errors can be unmarshalled to the passed interfaces
-func (c *Client) Execute(r Request, data interface{}, errors interface{}) error {
+func (c *Client) Execute(r Request, data interface{}) error {
 	payload, err := json.Marshal(r)
 	if err != nil {
 		return err
@@ -90,10 +125,12 @@ func (c *Client) Execute(r Request, data interface{}, errors interface{}) error 
 	}
 
 	if response.Errors != nil {
-		err = json.Unmarshal(*response.Errors, errors)
+		var errors Errors
+		err = json.Unmarshal(*response.Errors, &errors)
 		if err != nil {
 			return err
 		}
+		return errors
 	}
 
 	return nil
